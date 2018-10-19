2c1e5ac35e8e301b7034b9c419bedbe02733a84f
Aventerine <aventerine@gmail.com>
2018-10-18 22:42:56 -0400

    Added markers to map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+
+import axios from 'axios'
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount(){
-    this.renderMap();
+    this.getVenus();
   }
   
   renderMap = () => {
@@ -12,12 +19,43 @@ class App extends Component {
     window.initMap = this.initMap
   }
 
+  getVenus = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "51WWNTHABB5J114P3QLN1ZHKLY3PIGRHMF5AJSO5MPO42HTC",
+      client_secret: "RPZIBZ012VBJI3D1ASIAHLMTHT2N3AWZQV13C12FNSAFDDIM",
+      query: "food",
+      ll: "41.7356894, -83.54819979999999",
+      v: "20180323"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          venues: response.data.response.groups[0].items
+        }, this.renderMap())
+      })
+      .catch(error => {
+        console.log('ERROR! ' + error)
+      })
+  }
+
   initMap = () => {
-        const map = new window.google.maps.Map(document.getElementById('map'), {
-          center: {lat: 41.7356894, lng: -83.54819979999999},
-          zoom: 8
-        });
-      }
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.7356894, lng: -83.54819979999999},
+    zoom: 12
+    })
+
+    this.state.venues.map(myVenue => {
+      var marker = new window.google.maps.Marker({
+        position: {lat: myVenue.venue.location.lat, 
+                   lng: myVenue.venue.location.lng},
+        map: map,
+        title: myVenue.venue.name
+      })
+    })
+
+  }
 
   render() {
     return (
